48def9fb0ff9762674660823db9102201b19ff2d
DJ Idorot <ido20002@byui.edu>
2025-10-27 15:35:18 +0800

    updated

---
+++ NuGetDemo/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+using Humanizer;
+
+Console.WriteLine(3661.Seconds().Humanize()); // "1 hour, 1 minute, and 1 second"

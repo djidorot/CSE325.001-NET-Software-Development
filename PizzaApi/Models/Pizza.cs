bb3f4a72e952acf9abe341367b581795aff032f3
DJ Idorot <ido20002@byui.edu>
2025-10-27 15:58:14 +0800

    updated

---
+++ PizzaApi/Models/Pizza.cs
@@ -0,0 +1,9 @@
+namespace PizzaApi.Models;
+
+public class Pizza
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsGlutenFree { get; set; }
+    public decimal Price { get; set; }
+}

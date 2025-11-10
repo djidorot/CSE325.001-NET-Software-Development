68ddf3b3f9c0c401ff5f3200df1e7b257f104dad
DJ Idorot <ido20002@byui.edu>
2025-11-10 14:52:00 +0800

    updated

--- HelloCSharp/obj/Debug/net8.0/HelloCSharp.AssemblyInfo.cs
+++ HelloCSharp/obj/Debug/net8.0/HelloCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b889af78a4ae5ef1e50b684527373d9df1a997a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf8d5bba7d379ef3f38d1d0cf29003903b3c6fc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
